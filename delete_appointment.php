d27762a07d0a767e5ab945aa92d4e2f9b17597ea
trevunderwood <trev.underwood@gmail.com>
2023-05-04 17:29:51 -0500

    finished all code

--- delete_appointment.php
+++ delete_appointment.php
@@ -4,41 +4,37 @@
 </head>
 <body>
     <?php
-    // Check if appointment ID is set in the URL
-    if (!isset($_GET['APPT_ID'])) {
-        // If appointment ID is not set, redirect to appointments page
-        header("Location: appointments.php");
+    
+    if (!isset($_GET['APPT_ID'])) { // Check if appointment ID is in url
+        
+        header("Location: appointments.php"); //redirect back if not
         exit();
     }
 
-    // Store appointment ID from URL parameter
-    $appt_id = $_GET['APPT_ID'];
+    
+    $appt_id = $_GET['APPT_ID']; //get appt_id
 
-    // Check if confirmation form is submitted
-    if (isset($_POST['confirm'])) {
+    
+    if (isset($_POST['confirm'])) { //if form is submitted
         // Connect to database server
         $servername = "localhost";
         $username   = "root";
         $password   = "";
         $dbname     = "csce310";
 
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        $conn = mysqli_connect($servername, $username, $password, $dbname); //connect to database
 
-        // Check connection
         if (!$conn) {
             die("Connection failed: " . $conn->connect_error);
         }
 
-        // Delete appointment from database
-        $sql = "DELETE FROM appointments WHERE APPT_ID='$appt_id'";
-        $result = mysqli_query($conn, $sql);
+        $sql = "DELETE FROM appointments WHERE APPT_ID='$appt_id'"; //delete appointment for passed in appt_id
+        $result = mysqli_query($conn, $sql); //run query
 
         // Close database connection
         mysqli_close($conn);
 
-        // Redirect to appointments page
-        header("Location: appointments.php");
+        header("Location: appointments.php"); //redirect back to appointments page
         exit();
     }
     ?>
